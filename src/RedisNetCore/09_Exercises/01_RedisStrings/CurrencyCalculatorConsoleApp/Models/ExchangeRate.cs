25fa9e64ea2030ab5e36c5e6ad8285821a65e1c1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-01-15 10:38:20 +0100

    Dodanie przelicznika walut

---
+++ src/RedisNetCore/09_Exercises/01_RedisStrings/CurrencyCalculatorConsoleApp/Models/ExchangeRate.cs
@@ -0,0 +1,26 @@
+﻿namespace CurrencyCalculatorConsoleApp.Models;
+
+public class ExchangeRate
+{
+    public string table { get; set; }
+    public string currency { get; set; }
+    public string code { get; set; }
+    public Rate[] rates { get; set; }
+
+    public override string ToString()
+    {
+        return $"{table} {currency} {code}: {rates[0]}";
+    }
+}
+
+public class Rate
+{
+    public string no { get; set; }
+    public DateOnly effectiveDate { get; set; }
+    public decimal mid { get; set; }
+
+    public override string ToString()
+    {
+        return $"{effectiveDate} {mid}";
+    }
+}
---
+++ src/RedisNetCore/09_Exercises/01_RedisStrings/CurrencyCalculatorConsoleApp/Program.cs
@@ -0,0 +1,95 @@
+﻿
+using CurrencyCalculatorConsoleApp.Models;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using System.Net.Http.Json;
+
+Console.WriteLine("Hello, World!");
+
+// dotnet add package StackExchange.Redis
+
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddDebug());
+
+IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect("localhost:6379", options =>
+{
+   // options.ReconnectRetryPolicy = new LinearRetry(2000);
+    options.ConnectRetry = 5;
+    options.LoggerFactory = factory;
+});
+
+connectionMultiplexer.ConnectionFailed += (sender, e) =>
+{
+    if (e.ConnectionType == ConnectionType.Interactive)
+        Console.WriteLine($"ConnectionFailed: {e.FailureType}");
+};
+connectionMultiplexer.ConnectionRestored += (sender, e) =>
+{
+    if (e.ConnectionType == ConnectionType.Interactive)
+        Console.WriteLine($"ConnectionRestored: {e.FailureType}");
+};
+
+connectionMultiplexer.ErrorMessage += (sender, e) => Console.WriteLine(e.Message);
+
+var db = connectionMultiplexer.GetDatabase();
+
+const string table = "a";
+
+while (true)
+{
+    Console.Write("Podaj kod waluty: ");
+    var code = Console.ReadLine();
+
+    Console.Write("Podaj kwotę: ");
+
+    if (!decimal.TryParse(Console.ReadLine(), out decimal amount))
+    {
+        Console.WriteLine("Nieprawidłowy format");
+
+        continue;
+    }
+
+    // TODO: Pobierz z cache'a 
+    string key = $"exchangerates:{table}:{code}";
+
+    decimal rate;
+
+    RedisValue result;
+
+    try
+    {
+         result = await db.StringGetAsync(key);
+    }
+    catch (RedisConnectionException ex)
+    {
+        result = RedisValue.Null;
+    }
+
+    if (!result.HasValue)
+    {
+        var baseAddress = new Uri("https://api.nbp.pl/");
+
+        var url = $"api/exchangerates/rates/{table}/{code}?format=json";
+
+        HttpClient client = new HttpClient();
+        client.BaseAddress = baseAddress;
+
+        var exchangeRate = await client.GetFromJsonAsync<ExchangeRate>(url);
+
+        Console.WriteLine(exchangeRate);
+
+        rate = exchangeRate.rates[0].mid;
+
+        if (connectionMultiplexer.IsConnected)
+            await db.StringSetAsync(key, rate.ToString());
+    }
+    else
+    {
+        rate = decimal.Parse(result);
+    }
+
+
+    var convertedAmount = amount * rate;
+
+    Console.WriteLine(convertedAmount);
+
+}
