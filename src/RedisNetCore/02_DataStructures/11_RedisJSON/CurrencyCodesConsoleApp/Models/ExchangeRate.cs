28e364375ee5fdf54f5c07a448f340eddcc2ae35
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-01-15 13:30:41 +0100

    Dodanie klienta RedisJson

---
+++ src/RedisNetCore/02_DataStructures/11_RedisJSON/CurrencyCodesConsoleApp/Models/ExchangeRate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyCodesConsoleApp.Models;
+
+public class ExchangeRate
+{
+    public string table { get; set; }
+    public string currency { get; set; }
+    public string code { get; set; }
+    public Rate[] rates { get; set; }
+
+    public override string ToString()
+    {
+        return $"{table} {currency} {code}: {rates[0]}";
+    }
+}
+
+public class Rate
+{
+    public string no { get; set; }
+    public DateOnly effectiveDate { get; set; }
+    public decimal mid { get; set; }
+
+    public override string ToString()
+    {
+        return $"{effectiveDate} {mid}";
+    }
+}
---
+++ src/RedisNetCore/02_DataStructures/11_RedisJSON/CurrencyCodesConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using CurrencyCodesConsoleApp.Models;
+using NRedisStack;
+using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+using System.Net.Http.Json;
+
+Console.WriteLine("Hello, World!");
+
+
+// dotnet add package NRedisStack
+
+ConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect("localhost");
+IDatabase db = connectionMultiplexer.GetDatabase();
+
+while (true)
+{
+    Console.Write("Podaj symbol waluty: ");
+    var code = Console.ReadLine();
+
+
+    var baseAddress = new Uri("https://api.nbp.pl/");
+
+    const string table = "a";
+
+    var url = $"api/exchangerates/rates/{table}/{code}?format=json";
+
+    HttpClient client = new HttpClient();
+    client.BaseAddress = baseAddress;
+
+    var jsonResult = await client.GetStringAsync(url);
+
+
+    IJsonCommands json = db.JSON();
+
+    var key = $"exchangerates:rates:{table}:{code}";
+
+    json.Set(key, "$", jsonResult);
+
+    var result = json.Get(key);
+
+    var exchangeRate = json.Get<ExchangeRate>(key);
+
+    Console.WriteLine(exchangeRate);
+
+
+    var rate = exchangeRate.rates[0];
+
+    rate.effectiveDate = rate.effectiveDate.AddDays(1);
+    rate.mid = rate.mid * 1.1m;
+
+    json.ArrAppend(key, "$.rates", rate);
+
+
+}
+
+
+    
